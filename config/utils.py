3b187b48ef2666816d0290d9c772826f9633abc2
Jonathan Adly <jonathanadly@jonathans-air.myfiosgateway.com>
2021-10-17 11:10:55 -0400

    nav bar contact

--- config/utils.py
+++ config/utils.py
@@ -1,8 +1,8 @@
 from django.template.loader import render_to_string
 from django.core.mail import send_mail, EmailMessage
 
-DEFAULT_FROM_EMAIL = '"Jonathan From Django/HTMX" <jonathan@htmx-django.com>'
-REPLY_TO_EMAIL = "jonathan@htmx-django.com"
+DEFAULT_FROM_EMAIL = '"Jonathan From HTMX/Django" <hello@htmx-django.com>'
+REPLY_TO_EMAIL = "hello@htmx-django.com"
 
 
 def send_email(subject, message, to):
