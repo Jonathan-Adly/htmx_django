04bf9ecd04f7b0063e0f120c5dd9d6a3450f84c4
Jonathan Adly <jonathanadly@Jonathans-MacBook-Air.local>
2021-10-26 09:04:05 -0400

    email chunk

--- config/utils.py
+++ config/utils.py
@@ -6,22 +6,24 @@ REPLY_TO_EMAIL = "hello@htmx-django.com"
 
 
 def send_email(subject, message, to):
+    for i in range(0, len(to), 50):
+        chunk = to[i : i + 50]
+        html_template = "email/email_base.html"
+        html_message = render_to_string(
+            html_template,
+            {
+                "context": message,
+            },
+        )
+        email = EmailMessage(
+            subject,
+            html_message,
+            DEFAULT_FROM_EMAIL,  # from
+            [REPLY_TO_EMAIL],  # to
+            chunk,  # BCC will go here
+            reply_to=[REPLY_TO_EMAIL],
+        )
+        email.content_subtype = "html"
+        email.send()
 
-    html_template = "email/email_base.html"
-    html_message = render_to_string(
-        html_template,
-        {
-            "context": message,
-        },
-    )
-    email = EmailMessage(
-        subject,
-        html_message,
-        DEFAULT_FROM_EMAIL,  # from
-        [REPLY_TO_EMAIL],  # to
-        to,  # BCC will go here
-        reply_to=[REPLY_TO_EMAIL],
-    )
-    email.content_subtype = "html"
-    email.send()
     return None
